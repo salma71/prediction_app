a7118883cee75a19b545d3791a7f38901eff9aba
Salma Elshahawy <salmaeng71@gmail.com>
2020-09-24 22:42:02 -0400

    add version, add log, include log in the test console

--- packages/regression_model/regression_model/train_pipeline.py
+++ packages/regression_model/regression_model/train_pipeline.py
@@ -1,27 +1,22 @@
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
-import joblib
 
 from regression_model import pipeline
+from regression_model.processing.data_management import load_dataset, save_pipeline
 from regression_model.config import config
+from regression_model import __version__ as _version
+
+import logging
 
 
-def save_pipeline(*, pipeline_to_persist) -> None:
-    """Persist the pipeline."""
-
-    save_file_name = "regression_model.pkl"
-    save_path = config.TRAINED_MODEL_DIR / save_file_name
-    joblib.dump(pipeline_to_persist, save_path)
-
-    print("saved pipeline")
+_logger = logging.getLogger(__name__)
 
 
 def run_training() -> None:
     """Train the model."""
 
     # read training data
-    data = pd.read_csv(config.DATASET_DIR / config.TRAINING_DATA_FILE)
+    data = load_dataset(file_name=config.TRAINING_DATA_FILE)
 
     # divide train and test
     X_train, X_test, y_train, y_test = train_test_split(
@@ -33,6 +28,7 @@ def run_training() -> None:
 
     pipeline.price_pipe.fit(X_train[config.FEATURES], y_train)
 
+    _logger.info(f"saving model version: {_version}")
     save_pipeline(pipeline_to_persist=pipeline.price_pipe)
 
 
